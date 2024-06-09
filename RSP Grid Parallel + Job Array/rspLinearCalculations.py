4eb5dd1696b57e1a302f05649c512d4491729ea1
seIim <selim.kalici@yale.edu>
2024-06-08 20:07:21 -0400

    refactoring

--- RSP Grid Parallel + Job Array/rspLinearCalculations.py
+++ RSP Grid Parallel + Job Array/rspLinearCalculations.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd 
-from rsp_runner import *
+from rspLib import *
 
 # arraysize = int(os.environ['SLURM_ARRAY_TASK_COUNT'])
 # thisrank = int(os.environ['SLURM_ARRAY_TASK_ID'])
@@ -11,5 +11,8 @@ df = pd.read_csv('linear_grid.dat', sep='\s+')
 
 df = df[ df['model'] % arraysize == thisrank]
 
-print("These are the models I would run if this was real: \n")
-print(df)
+# print('These are the models I would run if this was real: \n')
+# print(df)
+
+os.chdir(os.environ['MESA_DIR'])
+# print(os.listdir())
